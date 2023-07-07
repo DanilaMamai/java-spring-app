7e2a7991efd72e936e25f49a2530f16fb40eef2d
Daniil Mamaenko <de_mamaenko@g.betboom.org>
2023-07-07 22:01:00 +0300

    init

---
+++ src/main/java/danilamamai/java_spring_app/App.java
@@ -0,0 +1,13 @@
+package danilamamai.java_spring_app;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
